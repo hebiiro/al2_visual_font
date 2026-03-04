7ac420da1ea4df243f6579bbb740b2d37705036a
hebiiro <io.hebiiro@gmail.com>
2026-03-04 18:34:04 +0900

    💥エクスポート関数も共通化

--- source/al2_visual_font/app.hpp
+++ source/al2_visual_font/app.hpp
@@ -5,17 +5,14 @@ namespace apn::visual_font
 	//
 	// このクラスはアプリケーションです。
 	//
-	inline struct app_t : app_interface_t
+	inline struct app_t : app_base_t
 	{
 		//
-		// dllの初期化処理を実行します。
+		// プラグインの初期化処理を実行します。
 		//
-		virtual BOOL dll_init() override
+		virtual BOOL on_initialize_plugin(DWORD version) override
 		{
 			MY_TRACE_FUNC("");
-
-			// 最初にaviutl2ウィンドウを捕捉します。
-			catch_aviutl2_window();
 
 			// 各種開始処理を実行します。
 			debug.init();
@@ -32,9 +29,9 @@ namespace apn::visual_font
 		}
 
 		//
-		// dllの後始末処理を実行します。
+		// プラグインの後始末処理を実行します。
 		//
-		virtual BOOL dll_exit() override
+		virtual BOOL on_uninitialize_plugin() override
 		{
 			MY_TRACE_FUNC("");
 
--- source/al2_visual_font/app_interface.hpp
+++ source/al2_visual_font/app_interface.hpp
@@ -5,14 +5,11 @@ namespace apn::visual_font
 	//
 	// このクラスはアプリケーションのインターフェイスです。
 	//
-	inline struct app_interface_t
+	inline struct app_interface_t : app_base_t
 	{
 		//
 		// コンストラクタです。
 		//
 		app_interface_t() { app = this; }
-
-		virtual BOOL dll_init() = 0;
-		virtual BOOL dll_exit() = 0;
 	} *app = nullptr;
 }
